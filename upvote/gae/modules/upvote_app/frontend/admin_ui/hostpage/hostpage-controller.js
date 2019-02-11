2d30210ec66b2d0a24402c6a2e01df2da46a6859
Alexander Mohr <thehesiod@hotmail.com>
2019-02-11 13:08:47 -0800

    bugfix

--- upvote/gae/modules/upvote_app/frontend/admin_ui/hostpage/hostpage-controller.js
+++ upvote/gae/modules/upvote_app/frontend/admin_ui/hostpage/hostpage-controller.js
@@ -268,7 +268,7 @@ upvote.admin.hostpage.HostController = class extends ModelController {
       this.card['rootScope'] = this.rootScope;
 
       // Hosts page doesn't have a card for now, so upload list results
-      this.content = [results];
+      this.content = [this.card];
       this.requestData['cursor'] = null;
       this.requestData['more'] = false;
       this.requestData['perPage'] = 50;
